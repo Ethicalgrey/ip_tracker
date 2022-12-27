04656186c540d1afeb315f7d0ce9b80de36e3b34
TECHIE <techie843@gmail.com>
2022-12-27 11:51:52 +0530

    Edited ip_tracker.py file

--- ip_tracker.py
+++ ip_tracker.py
@@ -1,16 +1,55 @@
-#!/bin/bash 
-os=$(uname -o)
-case $os in 
-  Android)
-    apt update && apt upgrade
-    pip install requests -y
-    apt install python3
-    echo "Now run the ip-tracker.py file"
-    ;;
-esac
-if [ "$os" != Android ]; then
-  sudo apt update && apt upgrade
-  sudo pip install requests
-  echo "Now run the ip-tracker.py file"
-fi 
-sleep 2
+import requests
+import os
+
+def erase():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+RED = '\033[91m'
+GREEN= '\033[92m'
+YELLOW= '\033[93m'
+BLUE= '\033[94m'
+MAGENTA= '\033[95m'
+CYAN= '\033[96m'
+WHITE= '\033[0m'
+
+
+
+erase()
+ip = input("Ip Address: ").strip()
+erase()
+
+def get_info():
+    res = requests.get(f"http://ip-api.com/json/{ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,mobile,proxy,hosting,query")
+
+    data = res.json()
+    status = res.status_code
+
+    ip_address = data["query"]
+    lon = float(data["lat"])
+    lat = float(data["lon"])
+    city = data["city"]
+    region = data["region"]
+    continent = data["continentCode"]
+    country = data["country"]
+    country_abbrev = data["countryCode"]
+    regionside = data["regionName"]
+    postal = data["zip"]
+    timezone = data["timezone"]
+    isp = data["isp"]
+    org = data["org"] 
+    asn = data["as"]
+
+    print(f'''    {WHITE}API status code: {RED}{status}\n
+    {WHITE}IP Address: {RED}{ip_address}
+    {WHITE}city: {RED}{city}
+    {WHITE}region: {RED}{regionside}, {WHITE}({RED}{region}{WHITE})
+    country: {RED}{country} {WHITE}({RED}{country_abbrev}{WHITE})
+    {WHITE}postal/zip: {RED}{postal}
+    {WHITE}timezone: {RED} {timezone}
+    {WHITE}ISP: {RED}{isp}
+    {WHITE}Provider: {RED}{org}
+    {WHITE}ASN: {RED}{asn}
+    {WHITE}Lat:{RED} {lat}\n    {WHITE}long:{RED} {lon}
+    ''')
+    
+get_info()
